02900bdae638a5237560b1533607cd26c9588137
jbt <jbt@404_INS>
2022-07-06 15:15:16 +0300

    15:15

--- spring-1/src/main/java/app/core/beans/ctors/App.java
+++ spring-1/src/main/java/app/core/beans/ctors/App.java
@@ -1,10 +1,20 @@
 package app.core.beans.ctors;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
 public class App {
 
 	public static void main(String[] args) {
 		
-
+		try(AnnotationConfigApplicationContext ctx=
+				new AnnotationConfigApplicationContext(App.class)){
+			
+			Teacher teacher = ctx.getBean(Teacher.class);
+			teacher.teach();
+			
+		}
 	}
 
 }
--- spring-1/src/main/java/app/core/beans/ctors/Teacher.java
+++ spring-1/src/main/java/app/core/beans/ctors/Teacher.java
@@ -1,9 +1,17 @@
 package app.core.beans.ctors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Teacher {
 
 	private ClassRoom classRoom;
+	@Autowired
 	private Silabus silabus;
+
+//	public Teacher() {
+//	}
 
 	public Teacher(ClassRoom classRoom, Silabus silabus) {
 		super();
@@ -11,8 +19,20 @@ public class Teacher {
 		this.silabus = silabus;
 	}
 
+	
+	public Teacher(Silabus silabus) {
+		super();
+		this.silabus = silabus;
+	}
+
+	@Autowired
+	public Teacher(ClassRoom classRoom) {
+		super();
+		this.classRoom = classRoom;
+	}
+
 	public void teach() {
-		System.out.println("hello class " + classRoom.getId() + " to we learn: " + silabus.getPlan());
+		System.out.println("hello class " + classRoom.getId() + " silabus: " + silabus.getPlan());
 	}
 
 }
