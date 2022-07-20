22bb88239c5d329cca8a6ff5b374b9c697829f0f
jbt <jbt@404_INS>
2022-07-20 15:30:45 +0300

    JPA

--- spring-boot-data-jpa/src/main/java/app/core/SpringBootDataJpaApplication.java
+++ spring-boot-data-jpa/src/main/java/app/core/SpringBootDataJpaApplication.java
@@ -2,6 +2,7 @@ package app.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
@@ -9,5 +10,7 @@ public class SpringBootDataJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
+	
+	
 
 }
