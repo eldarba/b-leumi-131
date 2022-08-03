75fb078fb4e48f035b16d2afae543fd7bd3f91e6
jbt <jbt@404_INS>
2022-08-03 15:17:52 +0300

    JPA + REST

--- spring-boot-data-jpa-mappings/src/main/java/app/core/controllers/SchoolController.java
+++ spring-boot-data-jpa-mappings/src/main/java/app/core/controllers/SchoolController.java
@@ -1,17 +1,21 @@
 package app.core.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import app.core.entities.School;
 import app.core.services.SchoolService;
 
+//@CrossOrigin(originPatterns = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("/api/school")
 public class SchoolController {
@@ -26,7 +30,11 @@ public class SchoolController {
 	
 	@GetMapping("/{id}")
 	public School getSchool(@PathVariable int id) {
-		return service.getSchool(id);
+		try {
+			return service.getSchool(id);
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+		}
 	}
 	
 	
