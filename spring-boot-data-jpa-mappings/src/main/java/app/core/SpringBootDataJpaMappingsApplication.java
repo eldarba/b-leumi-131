2eb34d214d475e51f3629e034a42fd36c4fbead5
jbt <jbt@404_INS>
2022-08-03 10:09:56 +0300

    AAA

---
+++ spring-boot-data-jpa-mappings/src/main/java/app/core/SpringBootDataJpaMappingsApplication.java
@@ -0,0 +1,13 @@
+package app.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaMappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaMappingsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-data-jpa-mappings/src/main/java/app/core/entities/Address.java
@@ -0,0 +1,33 @@
+package app.core.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "school")
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int number;
+	private String street;
+	private String city;
+	private String country;
+	
+	@OneToOne(mappedBy = "address") // inverse
+	private School school;
+	
+
+}
