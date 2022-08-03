3331aacc1217bb458e9274c62bfc2821873f7477
jbt <jbt@404_INS>
2022-08-03 13:03:53 +0300

    AAA

--- spring-boot-data-jpa-mappings/src/main/java/app/core/runners/Test1.java
+++ spring-boot-data-jpa-mappings/src/main/java/app/core/runners/Test1.java
@@ -12,9 +12,9 @@ import app.core.entities.School;
 import app.core.entities.Teacher;
 import app.core.services.SchoolService;
 
-@Component
+// @Component
 public class Test1 implements CommandLineRunner {
-	
+
 	@Autowired
 	private SchoolService service;
 
@@ -25,21 +25,16 @@ public class Test1 implements CommandLineRunner {
 		// the school teachers
 		List<Teacher> teachers = new ArrayList<>();
 		teachers.add(new Teacher(0, "Israel", null));
-		teachers.add(new Teacher(0, "Areala", null));
+		teachers.add(new Teacher(0, "Ariella", null));
 //		teachers.add(new Teacher(0, "Rachel", null));
 		// the school
 		School school = new School();
 		school.setName("JBT");
 		school.setAddress(address);
 		school.setTeachers(teachers);
-		
+
 		// add the above school to the database using the service
 		service.createSchool(school);
-		
-		
-		
-		
 	}
 
-	
 }
